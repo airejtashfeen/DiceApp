59ffdc6a48fc57f68dfbe79a14528e53a7b8017e
Airej Tashfeen <airejtashfeen@gmail.com>
2025-01-29 19:36:25 +0500

    All changes in the app committed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,34 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [UIImage(named: "DiceOne"),
+                     UIImage(named: "DiceTwo"),
+                     UIImage(named: "DiceThree"),
+                     UIImage(named: "DiceFour"),
+                     UIImage(named: "DiceFive"),
+                     UIImage(named: "DiceSix")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = diceArray[0]
+        diceImageView2.image = diceArray[0]
     }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        UIView.transition(with: diceImageView1, duration: 0.3, options: .transitionFlipFromLeft, animations: {
+            self.diceImageView1.image = self.diceArray[Int.random(in: 0...5)]
+        }, completion: nil)
+        
+        UIView.transition(with: diceImageView2, duration: 0.3, options: .transitionFlipFromRight, animations: {
+            self.diceImageView2.image = self.diceArray[Int.random(in: 0...5)]
+        }, completion: nil)
 
-
+        print("Dice rolled with animation!")
+    }
 }
-
